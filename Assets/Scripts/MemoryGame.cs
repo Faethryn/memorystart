b28779f8ee2ab10266416cb980c1451bfd827f8b
Faethryn <laurens.muylle@student.howest.be>
2022-03-18 09:20:06 +0100

    o

--- Assets/Scripts/MemoryGame.cs
+++ Assets/Scripts/MemoryGame.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,25 @@ public class MemoryGame : MonoBehaviour
     {
         _board = new MemoryBoard(3, 3);
         Debug.Log(_board.Tiles.Count);
+
+        _board.Tiles[1].PropertyChanged += (s, e) =>
+          {
+              string debugString = s.ToString() + e.PropertyName;
+              Debug.Log(debugString);
+          };
     }
 
     // Update is called once per frame
     void Update()
     {
+        foreach(Tile tile in _board.Tiles)
+        {
+            tile.MemoryCardId += 2;
+        }
         
     }
+
+   
+
+
 }
